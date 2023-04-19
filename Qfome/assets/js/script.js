1472ec180d90edfc4c6ba321ca92d5f78bac836d
WillianDosSantosCosta <103893908+WillianDosSantosCosta@users.noreply.github.com>
2023-04-18 21:39:38 -0300

    Delete Qfome directory

--- Qfome/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector(".menu");
-const links = document.querySelector(".links");
-
-menu.addEventListener("click", () => {
-  menu.classList.toggle('active');
-  links.classList.toggle('active');
-});
