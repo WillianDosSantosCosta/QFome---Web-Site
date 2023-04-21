92a0f98c07b3183afcd433e4286f9acce85d5433
WillianDosSantosCosta <103893908+WillianDosSantosCosta@users.noreply.github.com>
2023-04-21 19:19:29 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+/* Menu*/
 const menu = document.querySelector(".menu");
 const links = document.querySelector(".links");
 
@@ -5,3 +6,23 @@ menu.addEventListener("click", () => {
   menu.classList.toggle('active');
   links.classList.toggle('active');
 });
+
+/*Carrinho de compras*/
+const cartButton = document.getElementById('cart-shop');
+const cartContainer = document.getElementById('cart-container');
+
+cartButton.addEventListener('click', function() {
+  cartContainer.style.opacity = '1000';
+  cartContainer.style.display = 'flex'
+});
+
+const closeCartButton = document.getElementById('close-cart-button');
+const Cartczontainer = document.getElementById('cart-container');
+
+closeCartButton.addEventListener('click', function() {
+  cartContainer.style.display = 'none';
+});
+
+
+
+
